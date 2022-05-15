b1918a85a1bb5d6427b2b75ec461a2c02c1732d6
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-16 01:09:30 +0545

    Styling fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,18 @@
-import { useState } from 'react';
-import { io } from 'socket.io-client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Custom-styling
 import './App.scss';
 
+// pages
+import Home from './pages/Home';
+
 const App = () => {
-  const [text, setText] = useState('');
-  const [texts, setTexts] = useState([]);
-  const socket = io('http://localhost:9999');
-
-  const handleSend = (e) => {
-    e.preventDefault();
-    if (text) {
-      socket.emit('message', text);
-      setText('');
-    }
-  };
-
-  socket.on('message', (msg) => {
-    setTexts([...texts, msg]);
-  });
-
   return (
-    <div className='container'>
-      <ul id='messages'>
-        {texts.map((txt, index) => {
-          return <li key={index}>{txt}</li>;
-        })}
-      </ul>
-      <form id='form' onSubmit={handleSend}>
-        <input
-          id='input'
-          autoComplete='off'
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button type='submit'>Send</button>
-      </form>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+      </Routes>
+    </Router>
   );
 };
 
