a54c2291fe1f86132687237e82f330efb207ba57
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-16 01:11:24 +0545

    Cleaning up

--- server/app.js
+++ server/app.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const http = require('http');
-const { Server } = require('socket.io');
+// const { Server } = require('socket.io');
+
+// routes
+const homeRoutes = require('./routes/home.js');
 
 const app = express();
 dotenv.config();
@@ -17,28 +20,27 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.json('Welcome to Textster');
-});
+app.use('/', homeRoutes);
 
 const httpServer = http.createServer(app);
 
-const io = new Server(httpServer, {
-  cors: {
-    origin: 'http://localhost:3000',
-    credentials: true,
-  },
-});
+// creating a Socket.IO server
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   },
+// });
 
-io.on('connection', (socket) => {
-  socket.on('message', (msg) => {
-    socket.emit('message', msg);
+// io.on('connection', (socket) => {
+//   socket.on('message', (msg) => {
+//     socket.emit('message', msg);
+//   });
+// });
+
+mongoose.connect(CONNECTION_URL, () => {
+  console.log('Database connected');
+  httpServer.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 });
-
-// mongoose.connect(CONNECTION_URL, () => {
-// console.log('Database connected');
-httpServer.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-// });
