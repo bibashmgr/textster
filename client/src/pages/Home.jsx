9c40bb63570e188c72c079d9b13a732e47f1b2f1
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-16 01:10:22 +0545

    Adding Home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// components
+import Topbar from '../components/Topbar';
+import Main from '../components/Main';
+
+const Home = () => {
+  return (
+    <>
+      <Topbar />
+      <Main />
+    </>
+  );
+};
+
+export default Home;
