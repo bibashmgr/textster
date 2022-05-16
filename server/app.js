571085d9a9a5643ddce183ea5bd7f7bb864db368
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-16 16:13:51 +0545

    Adding routes

--- server/app.js
+++ server/app.js
@@ -4,15 +4,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const http = require('http');
-// const { Server } = require('socket.io');
 
-// routes
-const homeRoutes = require('./routes/home.js');
+// importing routes
+const indexRoutes = require('./routes/index.js');
 
 const app = express();
 dotenv.config();
 
-// constants
+// declaring constants
 const CONNECTION_URL = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 9999;
 
@@ -20,23 +19,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
 
-app.use('/', homeRoutes);
+app.use('/', indexRoutes);
 
+// creating http-server
 const httpServer = http.createServer(app);
-
-// creating a Socket.IO server
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: 'http://localhost:3000',
-//     credentials: true,
-//   },
-// });
-
-// io.on('connection', (socket) => {
-//   socket.on('message', (msg) => {
-//     socket.emit('message', msg);
-//   });
-// });
 
 mongoose.connect(CONNECTION_URL, () => {
   console.log('Database connected');
