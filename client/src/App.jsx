da7ab935b4bb45fe073bcd3c82da46c91a8a38fd
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-18 21:34:59 +0545

    Adding routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import './App.scss';
 
 // pages
 import Home from './pages/Home';
+import Chat from './pages/Chat';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path='/' exact element={<Home />} />
+        <Route path='/chat' exact element={<Chat />} />
       </Routes>
     </Router>
   );
