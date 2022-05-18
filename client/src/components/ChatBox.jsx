ea4494e33dbdd74e9ead48cd935360ce3b98fea5
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-18 21:34:46 +0545

    Cleaning up and fixing

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -11,6 +11,11 @@ import Messages from './Messages';
 import './styles/ChatBox.scss';
 
 const ChatBox = () => {
+  // handlers
+  const handleSend = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className='chatbox-container'>
       <div className='chatbox-navbar'>
@@ -19,16 +24,17 @@ const ChatBox = () => {
       <div className='chatbox-content'>
         <Messages />
       </div>
-      <form className='chatbox-form'>
+      <form className='chatbox-form' onSubmit={handleSend}>
         <input
           type='text'
           name='message'
           id='message'
           className='chatbox-form-input'
+          autoComplete='off'
         />
-        <div className='chatbox-form-btn'>
+        <button className='chatbox-form-btn' type='submit'>
           <RiSendPlaneFill style={{ cursor: 'pointer' }} />
-        </div>
+        </button>
       </form>
     </div>
   );
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,19 +1,78 @@
 import React from 'react';
+import { useState } from 'react';
+
+// icons
+import { BsFillChatSquareTextFill, BsFillPeopleFill } from 'react-icons/bs';
+
+// components
+import ConversationTab from './ConversationTab';
+import ContactTab from './ContactTab';
+import ProfileCard from './ProfileCard';
 
 // custom-styling
 import './styles/Main.scss';
 
-// components
-import Sidebar from './Sidebar';
-import ChatBox from './ChatBox';
+const Sidebar = () => {
+  // states
+  const [conversationOpen, setConversationOpen] = useState(true);
 
-const Main = () => {
+  // handlers
+  const switchConversation = () => {
+    setConversationOpen(true);
+  };
+  const switchContact = () => {
+    setConversationOpen(false);
+  };
+
   return (
     <div className='main-container'>
-      <Sidebar />
-      <ChatBox />
+      <div className='main-tabs'>
+        <div className='tabs'>
+          <div
+            className='tab-conversation'
+            id='conversation'
+            onClick={switchConversation}
+            style={
+              conversationOpen
+                ? {
+                    borderBottom: '5px solid #3AAFA9',
+                  }
+                : {
+                    border: 'none',
+                  }
+            }
+          >
+            <BsFillChatSquareTextFill
+              id='conversationicon'
+              onClick={switchConversation}
+            />
+          </div>
+          <div
+            className='tab-contact'
+            id='contact'
+            onClick={switchContact}
+            style={
+              !conversationOpen
+                ? {
+                    borderBottom: '5px solid #3AAFA9',
+                  }
+                : {
+                    border: 'none',
+                  }
+            }
+          >
+            <BsFillPeopleFill id='contacticon' onClick={switchContact} />
+          </div>
+        </div>
+        <div className='tabs-content'>
+          {conversationOpen ? <ConversationTab /> : <ContactTab />}
+        </div>
+      </div>
+      <div className='main-profile'>
+        <ProfileCard />
+      </div>
     </div>
   );
 };
 
-export default Main;
+export default Sidebar;
