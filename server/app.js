2866d49f452ad366372855e3a37e61d43507be74
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-22 19:08:12 +0545

    Udpating middlewares

--- server/app.js
+++ server/app.js
@@ -1,32 +1,75 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const http = require('http');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const http = require('http');
-
-// importing routes
-const indexRoutes = require('./routes/index.js');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+// const MongoStore = require('connect-mongo');
+const passport = require('passport');
 
 const app = express();
 dotenv.config();
 
-// declaring constants
-const CONNECTION_URL = process.env.MONGODB_URL;
+// environment-variables
+const CLIENT_URL = process.env.CLIENT_URL;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+// const CONNECTION_URL = process.env.MONGODB_LOCAL_URL;
+const CONNECTION_URL = process.env.MONGODB_CLOUD_URL;
 const PORT = process.env.PORT || 9999;
 
-app.use(cors());
+// config
+require('./config/passport.js');
+
+// middlewares
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ extended: true }));
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    // store: MongoStore.create({ mongoUrl: CONNECTION_URL }),
+  })
+);
+// app.use(passport.authenticate(session));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// routes
+const indexRoutes = require('./routes/index.js');
+const authRoutes = require('./routes/auth.js');
 
 app.use('/', indexRoutes);
+app.use('/auth', authRoutes);
 
 // creating http-server
 const httpServer = http.createServer(app);
 
-mongoose.connect(CONNECTION_URL, () => {
-  console.log('Database connected');
-  httpServer.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
-});
+mongoose.connect(
+  CONNECTION_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (error) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Database connected');
+    }
+    httpServer.listen(PORT, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(`Server running on http://localhost:${PORT}`);
+      }
+    });
+  }
+);
