83bce21f89e408fa0a8ffd3a2336661b42a77b56
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-25 22:44:56 +0545

    Udpating

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-// const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo');
 const passport = require('passport');
 
 const app = express();
@@ -14,8 +14,7 @@ dotenv.config();
 // environment-variables
 const CLIENT_URL = process.env.CLIENT_URL;
 const SESSION_SECRET = process.env.SESSION_SECRET;
-// const CONNECTION_URL = process.env.MONGODB_LOCAL_URL;
-const CONNECTION_URL = process.env.MONGODB_CLOUD_URL;
+const CONNECTION_URL = process.env.MONGODB_LOCAL_URL;
 const PORT = process.env.PORT || 9999;
 
 // config
@@ -35,7 +34,10 @@ app.use(
     secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    // store: MongoStore.create({ mongoUrl: CONNECTION_URL }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+    store: MongoStore.create({ mongoUrl: CONNECTION_URL }),
   })
 );
 // app.use(passport.authenticate(session));
@@ -45,9 +47,13 @@ app.use(passport.session());
 // routes
 const indexRoutes = require('./routes/index.js');
 const authRoutes = require('./routes/auth.js');
+const contactRoutes = require('./routes/contact.js');
+const conversationRoutes = require('./routes/conversation.js');
 
 app.use('/', indexRoutes);
 app.use('/auth', authRoutes);
+app.use('/contact', contactRoutes);
+app.use('/conversation', conversationRoutes);
 
 // creating http-server
 const httpServer = http.createServer(app);
