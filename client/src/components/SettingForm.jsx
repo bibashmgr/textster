d8b303616aadad0c2509598dc9d66ab67292dd69
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-30 00:52:07 +0545

    Creating Setting page fro updating user

--- client/src/components/SettingForm.jsx
+++ client/src/components/SettingForm.jsx
@@ -1,20 +1,69 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 // custom-styling
 import './styles/SettingForm.scss';
 
 const SettingForm = () => {
+  const { logger } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState('');
+
+  useEffect(() => {}, []);
+
   // handlers
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    let messages = {};
+    let pattern = /^[a-z0-9_.]+$/;
+
+    if (username === '') {
+      messages.error = 'Username is required';
+    } else if (username.length < 3) {
+      messages.error = 'Username is too short';
+    } else if (username.length > 10) {
+      messages.error = 'Username is too long';
+    } else if (!pattern.test(username)) {
+      messages.error = 'Invalid character entry';
+    }
+
+    if (Object.entries(messages).length === 0) {
+      axios
+        .put('/user/update', { id: logger._id, username: username })
+        .then((res) => {
+          if (res.status === 201) {
+            setUsername('');
+            navigate('/');
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   return (
     <div className='setting-form-container'>
       <form className='setting-form-box' onSubmit={handleSubmit}>
         <div className='form-username-container'>
-          <label htmlFor='username'>Enter username</label>
-          <input type='text' name='email' id='email' />
+          <label htmlFor='username'>Enter username:</label>
+          <input
+            type='text'
+            name='username'
+            id='username'
+            autoCapitalize='off'
+            autoComplete='off'
+            autoCorrect='off'
+            value={username}
+            onChange={handleChange}
+          />
         </div>
         <div className='form-button-container'>
           <input type='submit' value='Update' />
--- client/src/pages/Setting.jsx
+++ client/src/pages/Setting.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
 
 // components
 import Topbar from '../components/Topbar';
@@ -7,19 +11,30 @@ import SettingForm from '../components/SettingForm';
 // custom-styling
 import './styles/Layout.scss';
 
+// actions
+import { setLogger } from '../features/userSlice';
+
 const Setting = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get('/auth/login/success')
+      .then((res) => {
+        dispatch(setLogger(res.data));
+        res.data.username && navigate('/');
+      })
+      .catch((error) => {
+        console.log(error.response.data.message);
+      });
+  }, [dispatch, navigate]);
+
   return (
     <div className='container'>
       <div className='box'>
         <Topbar />
-        <div
-          style={{
-            marginTop: '100px',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
+        <div className='form-container'>
           <SettingForm />
         </div>
       </div>
