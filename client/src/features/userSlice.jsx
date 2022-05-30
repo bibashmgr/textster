ea6102e71c4bbaa27f11a4914d360e2f73ff69fb
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-31 01:22:12 +0545

    Adding states for storing user contacts

--- client/src/features/userSlice.jsx
+++ client/src/features/userSlice.jsx
@@ -2,6 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   logger: null,
+  userContacts: null,
 };
 
 export const counterSlice = createSlice({
@@ -11,9 +12,12 @@ export const counterSlice = createSlice({
     setLogger: (state, action) => {
       state.logger = action.payload;
     },
+    setUserContacts: (state, action) => {
+      state.userContacts = action.payload;
+    },
   },
 });
 
-export const { setLogger } = counterSlice.actions;
+export const { setLogger, setUserContacts } = counterSlice.actions;
 
 export default counterSlice.reducer;
