9e44831aba8467bf11e9cc6c5ece63733393fd2b
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-30 11:56:31 +0545

    Updating userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -7,9 +7,13 @@ const userSchema = mongoose.Schema(
     },
     email: {
       type: String,
+      unique: true,
     },
     username: {
       type: String,
+      min: 3,
+      max: 20,
+      unique: true,
       default: '',
     },
     firstname: {
