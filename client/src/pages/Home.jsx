62dd14da5c5d9c36506052db9cabda36df70e617
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-05-30 11:55:18 +0545

    Managing state

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
 
 // components
 import Topbar from '../components/Topbar';
@@ -7,7 +10,23 @@ import Main from '../components/Main';
 // custom-styling
 import './styles/Layout.scss';
 
+// actions
+import { setLogger } from '../features/userSlice';
+
 const Home = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get('/auth/login/success')
+      .then((res) => {
+        dispatch(setLogger(res.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [dispatch]);
+
   return (
     <div className='container'>
       <div className='box'>
