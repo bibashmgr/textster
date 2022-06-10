4aff67691fcdd425c61b823e520fcc3c08705be3
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-06-10 22:17:07 +0545

    Routes for getting user messages

--- server/routes/message.js
+++ server/routes/message.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 // model
+const Conversation = require('../models/conversation.js');
 const Message = require('../models/message.js');
 
 // environment-variables
@@ -10,5 +11,27 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const { getVerify } = require('../middlewares/verify.js');
 
 const router = express.Router();
+
+router.get('/:id', getVerify, async (req, res) => {
+  try {
+    const finalConversation = [];
+    const messages = await Message.find().sort({ createdAt: 1 });
+    const conversation = await Conversation.find({
+      members: { $in: [req.user._id.toString()] },
+    });
+    const filterConversation = conversation.map((convo) => {
+      if (convo.members.includes(req.params.id)) {
+        finalConversation.push(convo);
+      }
+    });
+    const filterMessages = messages.filter(
+      (message) =>
+        message.conversationId === finalConversation[0]._id.toString()
+    );
+    res.status(200).json(filterMessages);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 module.exports = router;
