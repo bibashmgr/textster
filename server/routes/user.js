55bfbf6387ed957f3bd3a724070ef0efcf1bf8de
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-06-10 22:17:19 +0545

    Routes for geeting userInfo

--- server/routes/user.js
+++ server/routes/user.js
@@ -8,29 +8,46 @@ const { getVerify } = require('../middlewares/verify.js');
 
 const router = express.Router();
 
-router.put('/update', getVerify, async (req, res) => {
-  User.findOne({ username: req.body.username }, (error, result) => {
-    if (error) throw error;
-    if (result) {
-      res.status(400).json({ message: 'Username already taken' });
+router.get('/:id', getVerify, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.status(200).json(user);
     } else {
-      User.findByIdAndUpdate(
-        req.user._id.toString(),
-        {
-          $set: { username: req.body.username },
-        },
-        { new: true },
-        (err, updatedUser) => {
-          if (err) throw err;
-          if (updatedUser) {
-            res.status(201).json(updatedUser);
-          } else {
-            res.status(400).json({ message: 'Something went wrong' });
-          }
-        }
-      );
+      res.status(404).json('User Not Found');
     }
-  });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.put('/update', getVerify, async (req, res) => {
+  try {
+    User.findOne({ username: req.body.username }, (error, result) => {
+      if (error) throw error;
+      if (result) {
+        res.status(400).json({ message: 'Username already taken' });
+      } else {
+        User.findByIdAndUpdate(
+          req.user._id.toString(),
+          {
+            $set: { username: req.body.username },
+          },
+          { new: true },
+          (err, updatedUser) => {
+            if (err) throw err;
+            if (updatedUser) {
+              res.status(201).json(updatedUser);
+            } else {
+              res.status(400).json({ message: 'Something went wrong' });
+            }
+          }
+        );
+      }
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
