39ca4a95939c67ea9aa2677499f7a4f13aa7eef6
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-06-11 21:12:33 +0545

    Adding: Routes for creating new message

--- server/routes/message.js
+++ server/routes/message.js
@@ -16,6 +16,31 @@ router.get('/:id', getVerify, async (req, res) => {
   try {
     const finalConversation = [];
     const messages = await Message.find().sort({ createdAt: 1 });
+    if (messages) {
+      const conversation = await Conversation.find({
+        members: { $in: [req.user._id.toString()] },
+      });
+      const filterConversation = conversation.map((convo) => {
+        if (convo.members.includes(req.params.id)) {
+          finalConversation.push(convo);
+        }
+      });
+      const filterMessages = messages.filter(
+        (message) =>
+          message.conversationId === finalConversation[0]._id.toString()
+      );
+      res.status(200).json(filterMessages);
+    } else {
+      res.status(200).json([]);
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post('/:id/create', async (req, res) => {
+  try {
+    const finalConversation = [];
     const conversation = await Conversation.find({
       members: { $in: [req.user._id.toString()] },
     });
@@ -24,11 +49,27 @@ router.get('/:id', getVerify, async (req, res) => {
         finalConversation.push(convo);
       }
     });
-    const filterMessages = messages.filter(
-      (message) =>
-        message.conversationId === finalConversation[0]._id.toString()
-    );
-    res.status(200).json(filterMessages);
+    if (finalConversation.length > 0) {
+      const newMessage = new Message({
+        conversationId: finalConversation[0]._id,
+        senderId: req.user._id.toString(),
+        text: req.body.text,
+      });
+      const savedMessage = await newMessage.save();
+      res.status(201).json({ message: 'Message sent' });
+    } else {
+      const newConversation = new Conversation({
+        members: [req.user._id.toString(), req.params.id],
+      });
+      const savedConversation = await newConversation.save();
+      const newMessage = new Message({
+        conversationId: savedConversation._id,
+        senderId: req.user._id.toString(),
+        text: req.body.text,
+      });
+      const savedMessage = await newMessage.save();
+      res.status(201).json({ message: 'Message sent' });
+    }
   } catch (error) {
     res.status(400).json(error);
   }
