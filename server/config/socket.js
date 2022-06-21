a9429c983d47c7f293bf7491fd529baad16d9959
Bibash Thapa Magar <bibashthapamagar21@gmail.com>
2022-06-21 09:29:48 +0545

    Add: socket in backend

--- server/config/socket.js
+++ server/config/socket.js
@@ -1,9 +1,17 @@
 module.exports = (io) => {
   io.on('connection', (socket) => {
-    console.log(`A user connected: ${socket.id}`);
+    const users = [];
+
+    console.log(`${socket.id} connected : ${new Date().toLocaleTimeString()}`);
+
+    socket.on('getUserId', (id) => {
+      console.log(id);
+    });
 
     socket.on('disconnect', () => {
-      console.log(`A user disconnected: ${socket.id}`);
+      console.log(
+        `${socket.id} disconnected : ${new Date().toLocaleTimeString()}`
+      );
     });
   });
 };
